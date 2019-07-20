aaa929754a116379e786c77af539a090ad9cab70
Flatiron School <email.the.anderson@gmail.com>
2019-07-20 19:38:04 -0400

    db fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# bittie = Tank.create!(name: "Bittie Mae", status: "Nominal")
+bittie = Tank.create!(name: "Bittie Mae", status: "Nominal")
 
-bittie = Tank.find(1)
+# # 
+# Crewmembers.destroy_all
+# Mechanic.destroy_all
+# Tank.destroy_all
 
-# brad = Crewmember.create!(name: "Brad Pitt", rank: "Sergeant", tank: bittie)
-# adam = Crewmember.create!(name: "Adam Malarky", rank: "Corporal", tank: bittie)
-# sadiq = Crewmember.create!(name: "Saqid Khan", rank: "Private", tank: bittie)
-# harry = Crewmember.create!(name: "Harry Khalet", rank: "Private", tank: bittie)
+brad = Crewmember.create!(name: "Brad Pitt", rank: "Sergeant", tank: bittie)
+adam = Crewmember.create!(name: "Adam Malarky", rank: "Corporal", tank: bittie)
+sadiq = Crewmember.create!(name: "Saqid Khan", rank: "Private", tank: bittie)
+harry = Crewmember.create!(name: "Harry Khalet", rank: "Private", tank: bittie)
 
-# francis = Mechanic.create!(name: "Francis Pescamish", tank: bittie)
+francis = Mechanic.create!(name: "Francis Pescamish", tank: bittie)
 
-# otto = Tank.create!(name: "Otto", status: "Repairing")
+otto = Tank.create!(name: "Otto", status: "Repairing")
 
-# heinz = Crewmember.create!(name: "Heinz Guderian", rank: "General", tank: otto)
-# uli = Crewmember.create!(name: "Uli Jamensch", rank: "Sergeant", tank: otto)
-# gerhart = Crewmember.create!(name: "Gerhart Richeter", rank: "Private", tank: otto)
-# nike = Crewmember.create!(name: "Nike Mizell", rank: "Private", tank: otto)
+heinz = Crewmember.create!(name: "Heinz Guderian", rank: "General", tank: otto)
+uli = Crewmember.create!(name: "Uli Jamensch", rank: "Sergeant", tank: otto)
+gerhart = Crewmember.create!(name: "Gerhart Richeter", rank: "Private", tank: otto)
+nike = Crewmember.create!(name: "Nike Mizell", rank: "Private", tank: otto)
 
-# roland = Mechanic.create!(name: "Roland Dollinger", tank: otto)
+roland = Mechanic.create!(name: "Roland Dollinger", tank: otto)
 
-# suka = Tank.create!(name: "Suka", status: "Nominal")
+suka = Tank.create!(name: "Suka", status: "Nominal")
 
-# grigori = Crewmember.create!(name: "Grigori Zhukov", rank: "General", tank: suka)
-# vassily = Crewmember.create!(name: "Vassily Rokosovsky", rank: "Sergeant", tank: suka)
-# pavel = Crewmember.create!(name: "Pavel Chekov", rank: "Private", tank: suka)
+grigori = Crewmember.create!(name: "Grigori Zhukov", rank: "General", tank: suka)
+vassily = Crewmember.create!(name: "Vassily Rokosovsky", rank: "Sergeant", tank: suka)
+pavel = Crewmember.create!(name: "Pavel Chekov", rank: "Private", tank: suka)
 
-# roland = Mechanic.create!(name: "Ilya Samusenko", tank: suka)
+roland = Mechanic.create!(name: "Ilya Samusenko", tank: suka)
 
 bilout = Tank.create!(name: "Bilout", status: "Nominal")
 
